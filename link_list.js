28b8964a89eae6cd62f77a509cc84bf0cb7e2d18
David Koy <dzilla398@gmail.com>
2025-08-22 14:16:08 +0930

    Created  class `Node` that will create Nodes for our Link List. In addtion created a class `LinkedList` which will implement the functions of a link lists

--- link_list.js
+++ link_list.js
@@ -1,7 +1,32 @@
 class LinkedList{
 
+    constructor(){ 
+    }
+    
+    // 1st do the following
+    // create the head node
+    // create the tail node
+    // create a node with value and nextaddr 
+    // link the node to the header
+
+    linkNode(value){
+
+    }
 }
 
+// Node contains data value and addr/next addr
 class Node{
     
+    createNode(value = null,nextAddr = null){
+        this.value = value
+        this.nextAddr = nextAddr
+    }
 }
+
+let n1 = new Node()
+let n2 = new Node()
+
+n1.createNode(5)
+n2.createNode(4)
+console.log(n1)
+console.log(n2)
