d30d175e23e7a131c2cea6b35734279068fe4c8b
David Koy <dzilla398@gmail.com>
2025-08-25 14:18:25 +0930

    Created a Node class that used in the `LinkedList` class to create new node objects for functions

--- link_list.js
+++ link_list.js
@@ -1,32 +1,39 @@
 class LinkedList{
-
-    constructor(){ 
-    }
     
-    // 1st do the following
-    // create the head node
-    // create the tail node
-    // create a node with value and nextaddr 
-    // link the node to the header
+    /**
+     * 
+     * Linked list will contain the following functions for a linked list 
+     * 1. `append(value)` adds a new node containing value to the end of the list
+     * 2. `prepend(value)` adds a new node containing value to the start of the list
+     * 3. `size` returns the total number of nodes in the list
+     * 4. `head` returns the first node in the list
+     * 5. `tail` returns the last node in the list
+     * 6. `at(index)` returns the node at the given index
+     * 7. `pop` removes the last element from the list
+     * 8. `contains(value)` returns true if the passed in value is in the list and otherwise returns false.
+     * 9. `find(value)` returns the index of the node containing value, or null if not found.
+     * 10. `toString` represents your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> null
+     */
+   
 
-    linkNode(value){
 
-    }
 }
 
-// Node contains data value and addr/next addr
+/**
+ * Node class contains following attributes.
+ * 1. data value 
+ * 2. next/addr 
+*/ 
 class Node{
-    
-    createNode(value = null,nextAddr = null){
+    constructor(value = null,nextAddr = null){
         this.value = value
         this.nextAddr = nextAddr
     }
 }
 
-let n1 = new Node()
-let n2 = new Node()
+let n1 = new Node(5)
+let n2 = new Node(4)
 
-n1.createNode(5)
-n2.createNode(4)
+n1.next = n2
 console.log(n1)
-console.log(n2)
+
