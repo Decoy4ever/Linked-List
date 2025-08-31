08dd85cb785a129c5f3c600bc591668e6156884e
David Koy <dzilla398@gmail.com>
2025-08-31 14:04:24 +0930

    Completed the implemntation of removing a node in link list by index.

--- link_list.js
+++ link_list.js
@@ -10,22 +10,21 @@ class Node{
     }
 }
 
+/**
+ * Linked list will contain the following functions for a linked list 
+ * 1. `append(value)` adds a new node containing value to the end of the list
+ * 2. `prepend(value)` adds a new node containing value to the start of the list
+ * 3. `size` returns the total number of nodes in the list
+ * 4. `head` returns the first node in the list
+ * 5. `tail` returns the last node in the list
+ * 6. `at(index)` returns the node at the given index
+ * 7. `pop` removes the last element from the list
+ * 8. `contains(value)` returns true if the passed in value is in the list and otherwise returns false.
+ * 9. `find(value)` returns the index of the node containing value, or null if not found.
+ * 10. `toString` represents your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> null
+ */
 class LinkedList{
-    
-    /**
-     * 
-     * Linked list will contain the following functions for a linked list 
-     * 1. `append(value)` adds a new node containing value to the end of the list
-     * 2. `prepend(value)` adds a new node containing value to the start of the list
-     * 3. `size` returns the total number of nodes in the list
-     * 4. `head` returns the first node in the list
-     * 5. `tail` returns the last node in the list
-     * 6. `at(index)` returns the node at the given index
-     * 7. `pop` removes the last element from the list
-     * 8. `contains(value)` returns true if the passed in value is in the list and otherwise returns false.
-     * 9. `find(value)` returns the index of the node containing value, or null if not found.
-     * 10. `toString` represents your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> null
-     */
+
     constructor(){
         this.head = null
     }
@@ -145,21 +144,14 @@ class LinkedList{
         }
 
         temp.nextNode = null
-
         return this.head
     }
 
     // 8. `contains(value)` returns true if the passed in value is in the list and otherwise returns false.
     contains(value){
         let current = this.head
-        console.log(current)
-
-        // traverese the link list
-        // if the value is equal to the current.value
-        // return true
-        // else return false
-
         let bool = false
+
         while(current !== null){
             if(value === current.value){
                 bool = true
@@ -167,14 +159,12 @@ class LinkedList{
                 bool
             }
             current = current.nextNode
-
         }
         return bool
     }
 
     // 9. `find(value)` returns the index of the node containing value, or null if not found.
     find(value){
-        // return the "index of the node" containing the value
         let current = this.head
         let index = 0
 
@@ -192,7 +182,7 @@ class LinkedList{
 
     }
 
-    //  inserts a new node with the provided value at the given index.
+    // Inserts a new node with the provided value at the given index.
     // 1. Traverse the current link list and find the node at index - 1
     // 2. Set the current.nextNode to point to the newly inserted node
     // 3. set the newly inserted node to the original node that was originally in the link list
@@ -202,10 +192,6 @@ class LinkedList{
         let node = new Node(value)
         let count = 0
 
-        if(index < 0){
-            
-        }
-
         // if the index is greater than size of link list return an error
         let sizeOfList = this.size()
         if(index > sizeOfList){
@@ -214,34 +200,46 @@ class LinkedList{
 
         let previous = null
         while(count < index && current !== null ){
-            console.log(current, count)
-
             previous = current
-            // console.log(previous)
             current = current.nextNode
             count++
         }
-        console.log(`\n`)
-        console.log(`Node at pos ${count}`)
-        console.log(`-------------`)
-        console.log(previous)
 
-        console.log(`\n`)
-        console.log(`new Node at pos ${count}`)
-        console.log(`-------------`)
-        // console.log(previous.nextNode)
         node.nextNode = previous.nextNode
-        console.log(node)
-
-        console.log(previous)
         previous.nextNode = node 
 
         // increase the size of the link list 
         sizeOfList++
 
-
         return this.head
       
+    }
+
+    // removeAt(index) that removes the node at the given index.
+    // traverse the list to find the node at index - 1 
+    // set node to null
+    // point the node at index - 1 to the next current node
+    removeAt(index){
+
+        let count = 0
+        let current = this.head 
+
+        let sizeOfList = this.size()
+        if(index > sizeOfList || sizeOfList === null){
+            throw new Error(`Out of Bounds`)
+        }
+
+        let previous = null
+        while(current !== null && count < index){
+            
+            previous = current
+            current = current.nextNode 
+            count++
+        }
+
+        previous.nextNode = previous.nextNode.nextNode
+        sizeOfList--
+        return this.head
     }
 
     toString(){
@@ -287,7 +285,10 @@ list.append("dingo");
 // list.append(3);
 
 // Prepend
-// list.prepend("hamster");
+list.prepend("hamster");
+console.log(`Original Link List`)
+console.log(list.toString())
+
 // list.prepend("turle");
 // list.prepend("monkey");
 
@@ -321,26 +322,16 @@ list.append("dingo");
 
 // insertAt()
 // repalce cat at index 1 with hamster
-console.log(list.insertAt(`hamster`,1))
-console.log(list.size())
+// console.log(list.insertAt(`hamster`,1))
+// console.log(list.size())
 
-console.log(list.insertAt(`chicken`,2))
-console.log(list.size())
+// console.log(list.insertAt(`chicken`,2))
+// console.log(list.size())
 
-console.log(list.insertAt(`chicken`,7))
+// console.log(list.insertAt(`chicken`,7))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// remove cat
+// console.log(list.removeAt(1))
+console.log(list.removeAt(2))
 console.log(list.toString())
 
